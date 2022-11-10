1ffe029aad5e21a4fd6fb6f876ba04b2a27ee8ed
Jsebaspl94 <juanseb_lp_94@hotmail.com>
2022-11-09 21:13:50 -0500

    Inicio Proyecto

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
