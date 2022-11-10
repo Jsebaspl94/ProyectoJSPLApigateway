4eeb06ea6b70e73cd083212e1eb24885e0cdbc4b
Jsebaspl94 <juanseb_lp_94@hotmail.com>
2022-11-09 21:37:04 -0500

    Avance Proyecto

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from flask import jsonify
 from flask import request
 from flask_cors import CORS
 import json
+from waitress import serve
+import datetime
+import requests
+import re
 
-
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+app=Flask(__name__)
+cors = CORS(app)
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"]="Server running ..."
+    return jsonify(json)
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
 
