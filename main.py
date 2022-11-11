42420823dae08b567e850cc37588dd7d9d2852dc
Jsebaspl94 <juanseb_lp_94@hotmail.com>
2022-11-10 23:59:48 -0500

    avance

--- main.py
+++ main.py
@@ -23,9 +23,49 @@ jwt = JWTManager(app)
 
 cors = CORS(app)
 
+@app.before_request
+def middleware():
+
+    urlCliente = request.path
+    metodoCliente = request.method
+
+
+    if (urlCliente == "/login"):
+
+        pass
+    else:
+
+        verify_jwt_in_request()
+
+        infoToken = get_jwt_identity()
+        idRol = infoToken["rol"]["_id"]
+
+        urlValidarPermiso = dataConfig["url-backend-security"] + "/permisos-rol/validar-permiso/rol/"+idRol
+
+        headers = {
+                "Content-Type": "application/json"
+            }
+        bodyRequest = {
+            "url": urlCliente,
+            "metodo": metodoCliente
+        }
+
+        responseValidarPermiso = requests.get(urlValidarPermiso, json=bodyRequest, headers=headers)
+        print("Status code del servicio validar permiso", responseValidarPermiso)
+
+        if (responseValidarPermiso.status_code == 200):
+
+            print("El cliente si tiene permisos")
+            pass
+        else:
+            return{"mensaje": "Permiso Denegado"}, 401
+
+
+
+
 @app.route("/login", methods=['POST'])
 def validarUsuario():
-
+    print("Entro a validar usuario")
     url = dataConfig["url-backend-security"] + "/usuarios/validar-usuario"
 
     headers = {
@@ -40,7 +80,7 @@ def validarUsuario():
         print("El usuario se valido correctamente")
         infoUsuario = response.json()
 
-        tiempoDelToken = datetime.timedelta(seconds=60)
+        tiempoDelToken = datetime.timedelta(seconds=60*60)
         newToken = create_access_token(identity=infoUsuario,expires_delta=tiempoDelToken)
 
         return {"token" : newToken}
@@ -48,8 +88,9 @@ def validarUsuario():
         return {"mensaje":"Usuario y contraseña Erroneos"}, 401
 
 
-@app.route("/crear-candidato", methods=['POST'])
-def crearEstudiante():
+@app.route("/candidato", methods=['POST'])
+def crearCandidato():
+
     url = dataConfig["url-backend-registraduria"] + "/candidato"
     headers = {
         "Content-Type": "application/json"
@@ -57,6 +98,18 @@ def crearEstudiante():
     body = request.get_json()
 
     response = requests.post(url, json=body, headers=headers)
+
+    return response.json()
+
+@app.route("/candidato/<string:idCandidato>", methods=['GET'])
+def buscarCandidato(idCandidato):
+    url = dataConfig["url-backend-registraduria"] + "/candidato/" + idCandidato
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+
+    response = requests.get(url, headers=headers)
 
     return response.json()
 
